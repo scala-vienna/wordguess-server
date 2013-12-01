bcf3731d54e01f8481a5b983b89d3105f0b1fc3c
Christian Papauschek <christian@papauschek.at>
2013-12-01 16:02:36 +0100

    made token string public

--- app/clashcode/logic/WordTokenizer.scala
+++ app/clashcode/logic/WordTokenizer.scala
@@ -2,12 +2,13 @@ package clashcode.logic
 
 import scala.util.parsing.combinator._
 
-class Token(str: String) {
+abstract class Token {
+  def str : String
   val length = str.length()
 }
 
-case class Word(str: String) extends Token(str)
-case class NonWord(str: String) extends Token(str)
+case class Word(str: String) extends Token
+case class NonWord(str: String) extends Token
 
 object WordTokenizer extends RegexParsers {
   override val skipWhitespace = false
