d59cab3d2827ea4b4e695d5a7d93354f32de59cc
Sebastian Nozzi <sebnozzi@gmail.com>
2013-12-02 21:21:51 +0100

    Not using re-connect button anymore (not reliable) Removed reset button (feature not implemented)

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -26,48 +26,19 @@ $(function() {
                 )
             })
         }
-        else if (data.game) {
-            var p1 = data.game[0]
-            var p2 = data.game[1]
-
-            var getColor = function(coop) {
-                if (coop === true) return "#ccff66";
-                else if (coop === false) return "#ffcc00";
-                return "#cccccc"
-            }
-
-            $('#games-table tbody').prepend(
-                $('<tr>')
-                    .append($('<td>').text(moment().format('HH:mm:ss')))
-                    .append($('<td>').text(p1.points))
-                    .append($('<td>').text(p1.name).css('background-color', getColor(p1.cooperate)))
-                    .append($('<td>').text(p2.name).css('background-color', getColor(p2.cooperate)))
-                    .append($('<td>').text(p2.points))
-            )
-
-            while ($('#games-table tbody tr').length > 15) {
-                $('#games-table tbody tr').last().remove()
-            }
-        }
-        else
+        else {
             console.log(data)
+        }
     }
 
     var websocket = new WebSocket(wsUri);
     websocket.onopen = function(evt) {
         update({ status: "Connected"})
-        $('#connect-button').prop("disabled", true);
     };
     websocket.onclose = function(evt) {
         update({ status: "Disconnected"})
-        $('#connect-button').prop("disabled", false);
     };
-
     websocket.onmessage = function(evt) { update(jQuery.parseJSON(evt.data)) };
     websocket.onerror = function(evt) { update({ status: evt.data}) };
-
-    $('#reset-button').click(function() {
-        websocket.send(JSON.stringify({ action: "reset" }));
-    })
 
 })
